d4ac188c51992a98df6c098987be33ebc1753d1e
JohnXaverius <lukas.suchy@outlook.com>
2024-11-13 22:48:13 +0100

    verze 0.12 Doplneno zakladni klikani

--- skript.js
+++ skript.js
@@ -1,6 +1,13 @@
 //Testové příkazy
-console.log("First word");
-document.body.innerHTML += "<p>JavaScript byl načten a funguje.</p>";
-alert("JavaScript je načtený!");
+//console.log("First word");
+//document.body.innerHTML += "<p>JavaScript byl načten a funguje.</p>";
+//alert("JavaScript je načtený!");
 //KONEC testu
 
+let ziskaneBody = 0;
+const clickButton = document.getElementById('click-button');
+const scoreDisplay = document.getElementById('hodnota');
+clickButton.addEventListener('click', () => {
+    ziskaneBody++;
+    scoreDisplay.textContent = ziskaneBody;
+});
