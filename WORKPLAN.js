1a3f4c6e8331ba82d2bf286d5d52bedc955df76e
JohnXaverius <lukas.suchy@outlook.com>
2024-11-21 21:58:33 +0100

    verze 0.18.2 dodan plan prace

---
+++ WORKPLAN.js
@@ -0,0 +1,8 @@
+// nečekat na první tick a rovnou ukazovat plavoucí okno
+// nechat zakryté upgrady dokud je poprvé nekoupím
+// rovnou ukazovat celkové IQ a IQps, aby se neposouval obraz
+// mít klikací obrázek oříznutý přesně podle něho, aby tam nebyl čtvercový rámeček
+// ve statistikách ukazovat celkové kliky, IQps, počet koupených upgradů celkem,
+// upravit hodnoty upgradů, kliků a generování IQ
+// ukládat progress do localStorage v JSON formátu
+// sifrovat localStorage a zamezit editaci JS kodů ze strany uživatelů
